e466ec3e63085b07048fa4e3da4f672c6d78274e
Nafibal <baliya363@gmail.com>
2023-03-04 23:29:21 +0700

    update soal prioritas 2

--- 10_Javascript Refreshment/praktikum/js/app.js
+++ 10_Javascript Refreshment/praktikum/js/app.js
@@ -1,23 +1,40 @@
 // DOM SELECTOR
 const form = document.querySelector(".form");
-const productNameInput = document.querySelector(".product_name");
-const productPriceInput = document.querySelector(".product_price");
+const productNameInput = document.querySelector("#product_name");
+const productPriceInput = document.querySelector("#product_price");
+const productDescInput = document.querySelector("#product_description");
 
 // Variables
 const MAX_CHAR_PER_INPUT = 25;
+const FORBIDDEN_CHAR_REGEX = /[@/#\{\}]/;
 
+// Functions
 const checkMaxInputChar = (element, maxChar) => {
   return element.value.length > maxChar;
 };
 const checkBlankInput = (element) => {
   return element.value == "";
 };
+const checkInputSpecificChars = (element, charRegex) => {
+  let regex = new RegExp(charRegex);
+  return regex.test(element.value);
+};
 
+// console.log(checkInputSpecificChars("hello world", /[@/#\{\}]/));
+
+// EventListeners
 form.addEventListener("submit", () => {
+  if (
+    checkBlankInput(productNameInput) ||
+    checkBlankInput(productPriceInput) ||
+    checkBlankInput(productDescInput)
+  ) {
+    alert("field must not be blank!!!");
+  }
   if (checkMaxInputChar(productNameInput, MAX_CHAR_PER_INPUT)) {
     alert("Product name must not exceed 25 characters!!!");
   }
-  if (checkBlankInput(productNameInput) || checkBlankInput(productPriceInput)) {
-    alert("field must not be blank!!!");
+  if (checkInputSpecificChars(productNameInput, FORBIDDEN_CHAR_REGEX)) {
+    alert("Name must not contain symbols!!!");
   }
 });
